27fa9b703ecc04ef2835d20728a92dbabdf0638a
Aleh Atsman <aleh.atsman@gmail.com>
2017-06-06 00:25:46 +0300

    test(handler): refactor basic http tests

--- test/vjobiway/handlers/handler_test.clj
+++ test/vjobiway/handlers/handler_test.clj
@@ -7,12 +7,12 @@
 (def app-component-instance (app-component {:db-component {}}))
 (def app (app-routes app-component-instance))
 
-(deftest test-app
-  (testing "main route"
+(deftest http-basic-test
+  (testing "h/hello-world should return hello-world text"
     (let [response (app (mock/request :get "/"))]
       (is (= (:status response) 200))
       (is (= (:body response) "hello-world"))))
 
-  (testing "not-found route"
+  (testing "h/not-found should return 404 on unknown route"
     (let [response (app (mock/request :get "/invalid"))]
       (is (= (:status response) 404)))))
