e7f322095181258a5e171b51412619d66624aba0
Aleh Atsman <aleh.atsman@gmail.com>
2017-06-27 00:51:47 +0300

    chore(plugins): add ultra dev plugin

--- project.clj
+++ project.clj
@@ -36,7 +36,8 @@
 
   :plugins [[lein-environ "1.1.0"]
             [lein-cljfmt "0.5.6"]
-            [lein-ancient "0.6.10"]]
+            [lein-ancient "0.6.10"]
+            [venantius/ultra "0.5.1"]]
 
   :profiles {:dev {:env {:database-url "jdbc:postgresql://localhost/jobs?user=postgres&password=mysecretpassword"}
                    :dependencies [[org.clojure/tools.namespace "0.2.11"]
