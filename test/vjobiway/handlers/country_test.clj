5666aaac262cf395623c11635fab1b0fa301acec
Aleh Atsman <aleh.atsman@gmail.com>
2017-06-27 00:51:32 +0300

    refactor(tests): create test-app helper

--- test/vjobiway/handlers/country_test.clj
+++ test/vjobiway/handlers/country_test.clj
@@ -1,23 +1,19 @@
 (ns vjobiway.handlers.country-test
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
-            [vjobiway.app-component :refer [app-component]]
             [vjobiway.db.country :as cdb]
-            [vjobiway.handlers.app-routes :refer [app-routes]]))
-
-(def app-component-instance (app-component {:db-component {}}))
-(def app (app-routes app-component-instance))
+            [vjobiway.util.http :refer [test-app]]))
 
 (deftest http-get-countries-test
   (testing "ch/get-countries should return countries"
     (with-redefs [vjobiway.db.country/get-countries (fn [db] [{:id 1 :country_code "BY"}])]
-      (let [response (app (mock/request :get "/api/countries"))]
+      (let [response (test-app (mock/request :get "/api/countries"))]
         (is (= 200 (:status response)))
         (is (= "[{\"id\":1,\"country_code\":\"BY\"}]" (:body response)))))))
 
 (deftest http-get-country-by-id-test
   (testing "ch/get-country-by-id should return 400 if id is valid"
     (with-redefs [vjobiway.db.country/get-country (fn [db id] {:id id})]
-      (let [response (app (mock/request :get "/api/countries/test"))]
+      (let [response (test-app (mock/request :get "/api/countries/test"))]
         (is (= 400 (:status response)))
         (is (= "{\"error\":\"id must be an integer\"}" (:body response)))))))
--- test/vjobiway/handlers/handler_test.clj
+++ test/vjobiway/handlers/handler_test.clj
@@ -1,18 +1,14 @@
 (ns vjobiway.handlers.handler-test
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
-            [vjobiway.app-component :refer [app-component]]
-            [vjobiway.handlers.app-routes :refer [app-routes]]))
-
-(def app-component-instance (app-component {:db-component {}}))
-(def app (app-routes app-component-instance))
+            [vjobiway.util.http :refer [test-app]]))
 
 (deftest http-basic-test
   (testing "h/hello-world should return hello-world text"
-    (let [response (app (mock/request :get "/"))]
+    (let [response (test-app (mock/request :get "/"))]
       (is (= (:status response) 200))
       (is (= (:body response) "hello-world"))))
 
   (testing "h/not-found should return 404 on unknown route"
-    (let [response (app (mock/request :get "/invalid"))]
+    (let [response (test-app (mock/request :get "/invalid"))]
       (is (= (:status response) 404)))))
